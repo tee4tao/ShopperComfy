3429ede0380d0ac11527c214ed380c1c2be460f2
taofeek-adeniyi <adeniyitaofeek001@gmail.com>
2024-01-11 20:58:23 +0100

    working on the nav page

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -18,7 +18,7 @@ const Nav = () => {
   return (
     <main className="fixed top-0 w-screen ">
       <nav className="w-full flex justify-center items-center shadow-2xl bg-Dark-nude">
-        <div className="nav-center flex flex-col justify-between items-center w-11/12">
+        <div className="nav-center flex flex-col justify-between items-center w-11/12 md:flex-row">
           <div className="nav-header flex justify-between items-center w-full p-2">
             <Link to={"home"} className="nav-logo">
               ShopperComfy
@@ -31,23 +31,23 @@ const Nav = () => {
             </button>
           </div>
           <div
-            className="links-container w-screen h-0 overflow-hidden ease-linear duration-300 bg-white"
+            className="links-container w-screen h-0 overflow-hidden ease-linear duration-300 bg-white md:bg-transparent"
             ref={linksContainerRef}
           >
             <div
-              className="links flex flex-col justify-around items-start w-11/12 space-y-2 p-2"
+              className="links flex flex-col justify-around items-start w-11/12 space-y-2 p-2 md:flex-row"
               ref={linksRef}
             >
               <Link
                 to={"home"}
-                className="capitalize text-lg text-Dark-nude w-screen hover:bg-Dark-nude hover:text-white ease-linear duration-300 hover:translate-x-4"
+                className="capitalize text-lg text-Dark-nude w-screen hover:bg-Dark-nude hover:text-white ease-linear duration-300 hover:translate-x-4 md:text-white"
               >
                 Home
               </Link>
-              <div className="capitalize text-lg text-Dark-nude w-screen hover:bg-Dark-nude hover:text-white ease-linear duration-300 hover:translate-x-4">
+              <div className="capitalize text-lg text-Dark-nude w-screen hover:bg-Dark-nude hover:text-white ease-linear duration-300 hover:translate-x-4 md:text-white">
                 Category
               </div>
-              <div className="capitalize text-lg text-Dark-nude w-screen  hover:bg-Dark-nude hover:text-white ease-linear duration-300 hover:translate-x-4 ">
+              <div className="capitalize text-lg text-Dark-nude w-screen  hover:bg-Dark-nude hover:text-white ease-linear duration-300 hover:translate-x-4  md:text-white">
                 Account
               </div>
             </div>
