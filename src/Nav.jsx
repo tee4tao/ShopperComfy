caa27222b7d11952dffc0cd7fe2f50601b33c925
taofeek-adeniyi <adeniyitaofeek001@gmail.com>
2024-01-11 18:22:38 +0100

    working on the nav page

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,10 +1,29 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
+import { FaBars } from "react-icons/fa";
 
 const Nav = () => {
   return (
-    <main className="fixed top-0 w-14 h-16 bg-Dark-nude">
-      <div>Nav</div>
+    <main className="fixed top-0 w-screen bg-Dark-nude">
+      <nav className="w-full flex justify-center items-center">
+        <div className="nav-center flex flex-col justify-between items-center w-11/12">
+          <div className="nav-header flex justify-between items-center w-full p-2">
+            <Link to={"home"} className="nav-logo">
+              ShopperComfy
+            </Link>
+            <button className="toggle-btn md:hidden">
+              <FaBars />
+            </button>
+          </div>
+          <div className="links-container h-0 overflow-hidden ease-linear duration-300">
+            <div className="links">
+              <Link to={"home"}>Home</Link>
+            </div>
+            <div>Category</div>
+            <div>Account</div>
+          </div>
+        </div>
+      </nav>
       <Outlet />
     </main>
   );
