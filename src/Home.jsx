8f56a41fecbb8f4328c440d832dd797dbc0db7a8
taofeek-adeniyi <adeniyitaofeek001@gmail.com>
2024-01-14 23:42:58 +0100

    working on the home page

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Home = () => {
-  return <main className="min-h-screen  w-screen"></main>;
+  const [isLoading, setIsLoading] = useState(true);
+  const [products, setProducts] = useState([]);
+  const getProduct = async (id) => {
+    try {
+      const resp = await fetch(`https://dummyjson.com/products`);
+      if (!resp.ok) {
+        throw new Error(`Something went wrong ${resp.status}`);
+      }
+      let product = await resp.json();
+      console.log(product);
+      setProducts(product);
+      setIsLoading(false);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  useEffect(() => {
+    getProduct();
+  }, []);
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <main className="min-h-screen  w-screen">
+      {products.products.map((items) => console.log(items))}
+    </main>
+  );
 };
 
 export default Home;
