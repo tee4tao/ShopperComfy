42da8179ec09901eedc2256ada30e57121fbdf1c
taofeek-adeniyi <adeniyitaofeek001@gmail.com>
2024-01-15 06:26:37 +0100

    worked on loading and error section of the home page

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,13 +1,18 @@
 import React, { useEffect, useState } from "react";
-
+import { Link } from "react-router-dom";
 const Home = () => {
   const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const [errMessage, setErrMessage] = useState(``);
   const [products, setProducts] = useState([]);
+  const loadedUser = localStorage.getItem("userDetail")
+    ? JSON.parse(localStorage.getItem("userDetail"))
+    : [];
   const getProduct = async (id) => {
     try {
       const resp = await fetch(`https://dummyjson.com/products`);
       if (!resp.ok) {
-        throw new Error(`Something went wrong ${resp.status}`);
+        throw new Error(`Something went wrong, ${resp.status}`);
       }
       let product = await resp.json();
       console.log(product);
@@ -15,13 +20,32 @@ const Home = () => {
       setIsLoading(false);
     } catch (err) {
       console.log(err);
+      setErrMessage(err.message);
+      setIsError(true);
+      setIsLoading(false);
     }
   };
   useEffect(() => {
     getProduct();
   }, []);
   if (isLoading) {
-    return <div>Loading...</div>;
+    return <div className="loader"></div>;
+  }
+  if (isError) {
+    return (
+      <div className="w-screen h-screen flex flex-col justify-center items-center">
+        <h1 className="text-4xl font-bold text-Dark-nude mb-4">Oops!</h1>
+        <p className="text-Dark-nude md:text-2xl mb-4">
+          Sorry, an unexpected error has occurred.
+        </p>
+        <p className="text-Dark-nude md:text-2xl mb-4">
+          <i>{errMessage}</i>
+        </p>
+        <button className=" border-4 text-white text-xl w-48 bg-Dark-nude p-2 rounded-xl ease-linear duration-300 hover:bg-white hover:text-Dark-nude mt-8">
+          <a href="javascript: location.reload();">Reload this page</a>
+        </button>
+      </div>
+    );
   }
   return (
     <main className="min-h-screen  w-screen">
