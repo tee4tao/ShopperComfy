3e69bb1e46494182fccbb92d29c48bc5d8135a2d
Taofeek Adeniyi <105851811+tee4tao@users.noreply.github.com>
2024-02-21 06:59:31 +0100

    working on account page

--- src/Account.jsx
+++ src/Account.jsx
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import placeholder from "./images/placeholder.png";
 
 const Account = () => {
-  return <div>Account</div>;
+  const loadedUser = localStorage.getItem("userDetail")
+    ? JSON.parse(localStorage.getItem("userDetail"))
+    : [];
+  const [imgSrc, setImgSrc] = useState(placeholder);
+  const [firstName, setFirstName] = useState(loadedUser.name);
+  return (
+    <main className=" w-full grid place-items-center mt-8 ">
+      <section className="flex flex-col items-center w-full md:max-w-4xl">
+        <div>
+          <img
+            src={imgSrc}
+            alt=""
+            className="bg-red-600 h-40 w-40 rounded-full "
+          />
+        </div>
+        <form
+          action=""
+          className="flex flex-col rounded-lg shadow-lg ease-linear duration-300 hover:shadow-2xl p-4"
+        >
+          {/* <div className=" flex flex-col"> */}
+          <label htmlFor="firstName" className="text-xl mr-8">
+            Name:
+          </label>
+          <input
+            type="text"
+            id="firstName"
+            name="FirstName"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+            className="border-2 mb-4 border-black rounded-md text-xl ease-linear duration-300 text-center"
+          />
+          {/* </div> */}
+        </form>
+      </section>
+    </main>
+  );
 };
 
 export default Account;
