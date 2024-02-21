cc70d01e8e3c18970262494cb4c89399edfbeb12
Taofeek Adeniyi <105851811+tee4tao@users.noreply.github.com>
2024-02-21 07:02:36 +0100

    working on the account page

--- src/Account.jsx
+++ src/Account.jsx
@@ -11,18 +11,14 @@ const Account = () => {
     <main className=" w-full grid place-items-center mt-8 ">
       <section className="flex flex-col items-center w-full md:max-w-4xl">
         <div>
-          <img
-            src={imgSrc}
-            alt=""
-            className="bg-red-600 h-40 w-40 rounded-full "
-          />
+          <img src={imgSrc} alt="" className="h-40 w-40 rounded-full " />
         </div>
         <form
           action=""
-          className="flex flex-col rounded-lg shadow-lg ease-linear duration-300 hover:shadow-2xl p-4"
+          className="flex flex-col w-11/12 rounded-lg shadow-lg ease-linear duration-300 hover:shadow-2xl p-4"
         >
           {/* <div className=" flex flex-col"> */}
-          <label htmlFor="firstName" className="text-xl mr-8">
+          <label htmlFor="firstName" className="text-xl mb-2">
             Name:
           </label>
           <input
