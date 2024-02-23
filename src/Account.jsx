99ee121920120ec2b287cfc288021a84a9ac7300
Taofeek Adeniyi <105851811+tee4tao@users.noreply.github.com>
2024-02-23 12:02:24 +0100

    working on the edit button in the account page

--- src/Account.jsx
+++ src/Account.jsx
@@ -6,7 +6,18 @@ const Account = () => {
     ? JSON.parse(localStorage.getItem("userDetail"))
     : [];
   const [imgSrc, setImgSrc] = useState(placeholder);
-  const [firstName, setFirstName] = useState(loadedUser.name);
+  const [userFirstName, setUserFirstName] = useState(loadedUser.name);
+  const [userEmail, setUserEmail] = useState(loadedUser.email);
+  const [userUsername, setUserUsername] = useState(loadedUser.username);
+  const [edit, setEdit] = useState(true);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setEdit(true);
+  };
+  const handleEdit = (e) => {
+    e.preventDefault();
+    setEdit(false);
+  };
   return (
     <main className=" w-full grid place-items-center mt-8 ">
       <section className="flex flex-col items-center w-full md:max-w-4xl">
@@ -15,21 +26,64 @@ const Account = () => {
         </div>
         <form
           action=""
-          className="flex flex-col w-11/12 rounded-lg shadow-lg ease-linear duration-300 hover:shadow-2xl p-4"
+          // onSubmit={handleSubmit}
+          className="flex flex-col w-3/5 rounded-lg shadow-lg ease-linear duration-300 hover:shadow-2xl p-4"
         >
-          {/* <div className=" flex flex-col"> */}
-          <label htmlFor="firstName" className="text-xl mb-2">
+          <label htmlFor="userFirstName" className="text-xl mb-2">
             Name:
           </label>
           <input
             type="text"
-            id="firstName"
-            name="FirstName"
-            value={firstName}
-            onChange={(e) => setFirstName(e.target.value)}
-            className="border-2 mb-4 border-black rounded-md text-xl ease-linear duration-300 text-center"
+            id="userFirstName"
+            name="userFirstName"
+            value={userFirstName}
+            onChange={(e) => !edit && setUserFirstName(e.target.value)}
+            className="border-2 mb-4 border-black rounded-md text-xl ease-linear duration-300 capitalize"
           />
-          {/* </div> */}
+          <label htmlFor="userEmail" className="text-xl my-2">
+            Email:
+          </label>
+          <input
+            type="email"
+            id="userEmail"
+            name="userEmail"
+            value={userEmail}
+            onChange={(e) => setUserEmail(e.target.value)}
+            className="border-2 mb-4 border-black rounded-md text-xl ease-linear duration-300 lowercase"
+          />
+          <label htmlFor="userUsername" className="text-xl mb-2">
+            Username:
+          </label>
+          <input
+            type="text"
+            id="userUsername"
+            name="userUsername"
+            value={userUsername}
+            onChange={(e) => setUserUsername(e.target.value)}
+            className="border-2 mb-4 border-black rounded-md text-xl ease-linear duration-300 capitalize"
+          />
+          {edit ? (
+            <button
+              onClick={handleEdit}
+              className="border-2 w-3/5 bg-Dark-nude text-white h-10 rounded-md p-2 text-xl flex items-center justify-center mt-12 mx-auto ease-linear duration-300 hover:text-Dark-nude hover:bg-white"
+            >
+              Edit
+            </button>
+          ) : (
+            <button
+              type="submit"
+              onClick={handleSubmit}
+              className="border-2 w-3/5 bg-Dark-nude text-white h-10 rounded-md p-2 text-xl flex items-center justify-center mt-12 mx-auto ease-linear duration-300 hover:text-Dark-nude hover:bg-white"
+            >
+              Save
+            </button>
+          )}
+          {/* <button
+            type="submit"
+            className="border-2 w-3/5 bg-Dark-nude text-white h-10 rounded-md p-2 text-xl flex items-center justify-center mt-12 mx-auto ease-linear duration-300 hover:text-Dark-nude hover:bg-white"
+          >
+            Save
+          </button> */}
         </form>
       </section>
     </main>
