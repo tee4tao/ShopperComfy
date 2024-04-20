8b895fc463b2eb31ff4a243172c8d6a27fc66c47
taofeek-adeniyi <adeniyitaofeek001@gmail.com>
2024-04-20 22:40:03 +0100

    testing

--- src/Paystack.jsx
+++ src/Paystack.jsx
@@ -91,7 +91,7 @@ const Paystack = () => {
         <div className="item">
           <div className="item-details">
             <p className="text-xl p-3 font-bold text-Dark-nude">
-              NGN {(amount / 100).toLocaleString()}
+              {(amount / 100).toLocaleString()}
             </p>
           </div>
         </div>
