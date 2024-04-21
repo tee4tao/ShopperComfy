0d81fc01adff163951c35a4b8cf134cc914feed5
taofeek-adeniyi <adeniyitaofeek001@gmail.com>
2024-04-21 01:32:46 +0100

    debugging

--- src/Paystack.jsx
+++ src/Paystack.jsx
@@ -58,7 +58,7 @@ const Paystack = () => {
     : [];
   let totalCost = localStorage.getItem(`totalcost`);
   const publicKey = "pk_test_966df8c4dafebc42e4007bdaf9453268d7e1fa29";
-  let amount = parseFloat(totalCost * 100 * rate.toFixed(2));
+  let amount = parseFloat((totalCost * 100 * rate).toFixed(2));
   // alert(typeof amount);
   console.log(amount);
   const [email, setEmail] = useState("");
@@ -92,8 +92,7 @@ const Paystack = () => {
         <div className="item">
           <div className="item-details">
             <p className="text-xl p-3 font-bold text-Dark-nude">
-              NGN 2000
-              {/* NGN {(amount / 100).toLocaleString()} */}
+              NGN {(amount / 100).toLocaleString()}
             </p>
           </div>
         </div>
